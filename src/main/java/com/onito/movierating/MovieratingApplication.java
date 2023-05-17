8d1cf0aed1617cdbca62740d9d1b15fd8d275b82
Shalu <toshalusasi@gmail.com>
2023-05-17 14:34:52 +0530

    Spring Starter Project
    Spring Starter Project without impementation

---
+++ src/main/java/com/onito/movierating/MovieratingApplication.java
@@ -0,0 +1,13 @@
+package com.onito.movierating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieratingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieratingApplication.class, args);
+	}
+
+}
